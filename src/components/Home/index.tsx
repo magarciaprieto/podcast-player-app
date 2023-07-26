4eaf58c467f2c1f2c163aed97706d76ff36d1777
magarciaprieto <magarciaprieto@gmail.com>
2023-07-26 11:27:48 +0200

    feat: Add podcasts fetching + ListItem props

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
 import ListItem from '../ListItem'
+import { PodcastType } from '../../types'
+import styles from './index.module.css'
 
 const Home = () => {
+  const [podcasts, setPodcasts] = React.useState<PodcastType[]>([])
+
+  React.useEffect(() => {
+    const fetchPodcasts = async () => {
+      try {
+        const response = await fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
+        const data = await response.json()
+        const newData = data.feed.entry.map((item: any) => {
+          return {
+            title: item.title.label,
+            image: item['im:image'][2].label,
+            author: item['im:artist'].label,
+            id: item.id.attributes['im:id']
+          }
+        })
+        setPodcasts(newData)
+      } catch (error) {
+        console.error('Error fetching podcasts:', error)
+      }
+    }
+
+    fetchPodcasts()
+  }, [])
+
   return (
     <div>
-      <ListItem />
+      <h1>Top 100 Podcasts</h1>
+      <div className={styles.listContainer}>
+        {podcasts.map((podcast, idx) => (
+          <ListItem key={idx} podcast={podcast} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable quotes */
+import { PodcastType } from '../../types'
 import styles from './index.module.css'
 
-const ListItem = () => {
+interface ListItemProps {
+  podcast: PodcastType
+}
+const ListItem = ({ podcast }: ListItemProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.rectangle}>
         <div className={styles.empty} />
         <div className={styles.text}>
-          <div className={styles.title}>Song Exploder</div>
-          <div className={styles.author}>Author: NPR</div>
+          <div className={styles.title}>{podcast.title}</div>
+          <div className={styles.author}>Author: {podcast.author}</div>
         </div>
 
-        <div className={styles.circle}>
-          <img className={styles.image} src='https://miro.medium.com/v2/resize:fit:720/format:webp/1*8FkvzbSdSJ4HNxtuZo5kLg.jpeg' alt='' />
-        </div>
+      </div>
+      <div className={styles.circle}>
+        <img className={styles.image} src={podcast.image} alt='' />
       </div>
     </div>
   )
