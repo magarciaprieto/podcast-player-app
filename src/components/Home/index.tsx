92e01c3c5e5c2f29239cd4d448a43d5ed391441b
magarciaprieto <magarciaprieto@gmail.com>
2023-07-28 23:31:20 +0200

    feature: Add Icon + FIlter to Home component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ListItem from '../ListItem'
 import styles from './index.module.css'
 import usePodcastsFetch from '../../hooks/usePodcastsFetch'
+import Top100PodcastsIcon from '../Top100PodcastsIcon'
 
 interface HomeProps {
-  onLoading: (value: boolean) => void
+  onLoading: (value: boolean) => void;
 }
+
 const Home = ({ onLoading }: HomeProps) => {
   const { podcasts, isFetching } = usePodcastsFetch()
+  const [searchQuery, setSearchQuery] = useState('')
 
   React.useEffect(() => {
     onLoading(isFetching)
   }, [isFetching, onLoading])
 
+  const filteredPodcasts = React.useMemo(() => {
+    if (!searchQuery) {
+      return podcasts
+    }
+
+    const searchTerm = searchQuery.toLowerCase()
+    return podcasts.filter(
+      (podcast) =>
+        podcast.title.toLowerCase().includes(searchTerm) ||
+        podcast.author.toLowerCase().includes(searchTerm)
+    )
+  }, [podcasts, searchQuery])
+
   return (
     <div>
+      <div className={styles.searchContainer}>
+        <Top100PodcastsIcon />
+        <input
+          className={styles.filter}
+          type='text'
+          placeholder='Filter podcasts...'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
       <div className={styles.listContainer}>
-        {podcasts.map((podcast, idx) => (
+        {filteredPodcasts.map((podcast, idx) => (
           <ListItem key={idx} podcast={podcast} />
         ))}
       </div>
---
+++ src/components/Top100PodcastsIcon/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css'
+
+const Top100PodcastsIcon = () => {
+  return (
+    <div className={styles.pill}>
+      <p className={styles.iconText}>
+        100
+      </p>
+    </div>
+  )
+}
+
+export default Top100PodcastsIcon
